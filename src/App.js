1395c91c29c20687d8ebd6d8f1172429bc3ae23c
Reiji777 <ld3576@gmail.com>
2019-02-14 15:02:24 +0900

    Created coupon page

--- src/App.js
+++ src/App.js
@@ -3,43 +3,46 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as actions from "./actions/actions.js";
 import "./App.scss";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import createHistory from "history/createBrowserHistory";
-const history = createHistory();
+import ShopPage from "./components/shopPage";
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import createHistory from "history/createBrowserHistory";
+// const history = createHistory();
 
 class App extends Component {
   constructor(props) {
     super(props);
     props.actions.fetchInit();
     this.ShopList = this.ShopList.bind(this);
-    this.showShopLogo=this.showShopLogo.bind(this);
+    this.showShopLogo = this.showShopLogo.bind(this);
   }
 
   ShopList({ coupons }) {
     return (
-      <Router history={history}>
+      <Router>
         <div>
-          {coupons.map(elem => this.showShopLogo(elem))}
+          {coupons.map(this.showShopLogo)}
+          <Route path="/:shopName" component={ShopPage} />
         </div>
       </Router>
-      )
+    );
   }
-    
+
   showShopLogo(elem) {
-      let {shopName, shopLogo} = elem;
-      console.log("got here");
-      console.log(elem);
-      return ( 
-      <Link to={`/${shopName}`}><img className="shopLogo" src={shopLogo} alt={shopName} /></Link>
-      )
+    let { shopName, shopLogo } = elem;
+    return (
+      <Link key={shopName} to={`/${shopName}`}>
+        <img className="shopLogo" src={shopLogo} alt={shopName} />
+      </Link>
+    );
   }
 
   render() {
-    let {ShopList} = this;
+    let { ShopList } = this;
     return (
       <div className="App">
         <header className="App-header">
-          <div className="ShopList">
+          <div className="shopList">
             {this.props.coupons ? (
               <ShopList coupons={this.props.coupons} />
             ) : (
---
+++ src/components/shopPage.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "../App.scss";
+class ShopPage extends Component {
+  setTitle(title) {
+    document.title = title;
+  }
+
+  ShowCouponList({ shop }) {
+    let coupons = shop.couponList.map((coupon, index) => {
+      return (
+        <div key={index} className="coupon">
+          {/* {shop.shopName} */}
+          <div className="couponImg">
+            <img className="couponImg" src={coupon.imgSrc} alt={index} />
+          </div>
+          <div className="couponText">
+            <p>{coupon.item}</p>
+          </div>
+        </div>
+      );
+    });
+    return <div className="couponList">{coupons}</div>;
+  }
+
+  render() {
+    this.setTitle(this.props.match.params.shopName);
+    let { ShowCouponList } = this;
+    let [shop] = this.props.coupons.filter(
+      shop => shop.shopName === this.props.match.params.shopName
+    );
+    return <ShowCouponList shop={shop} />;
+  }
+}
+
+export default connect(
+  state => ({ coupons: state.coupons }),
+  null
+)(ShopPage);
